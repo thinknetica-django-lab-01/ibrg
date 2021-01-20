ac2d4a6d32e8a125593f306c216a08eee192e1dd
ibrg <romanbezh@gmail.com>
2021-01-20 19:06:58 +0200

    удалил неиспользуемые импорти с admin.py

--- main/admin.py
+++ main/admin.py
@@ -2,9 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.contrib.flatpages.admin import  FlatpageForm, FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
-from ckeditor.fields import RichTextField
 from ckeditor.widgets import CKEditorWidget
-from django.db import models
 
 
 class CkeditorFlatpageForm(FlatpageForm):
