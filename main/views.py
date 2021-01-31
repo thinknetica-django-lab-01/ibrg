09ca08b8de6a592282afd6cfdd1ebad847b80173
ibrg <romanbezh@gmail.com>
2021-01-31 23:07:48 +0200

    удалена неиспользуемая функция

--- main/views.py
+++ main/views.py
@@ -16,25 +16,6 @@ class AdvertListView(ListView):
         if self.kwargs.get('category_slug'):
             queryset = queryset.filter(advert_category__category_slug=self.kwargs['category_slug']).order_by('-id')
         return queryset
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if not context.get('is_paginated', False):
-            return context
-
-        paginator = context.get('paginator')
-        num_pages = paginator.num_pages
-        current_page = context.get('page_obj')
-        page_no = current_page.number
-
-        if num_pages <= 11 or page_no <= 6:  # case 1 and 2
-            pages = [x for x in range(1, min(num_pages + 1, 12))]
-        elif page_no > num_pages - 6:  # case 4
-            pages = [x for x in range(num_pages - 10, num_pages + 1)]
-        else:  # case 3
-            pages = [x for x in range(page_no - 5, page_no + 6)]
-
-        context.update({'pages': pages})
-        return context
 
 
 
