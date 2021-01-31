8d58bd67d9ba570ac76acf8001f48e864a763553
ibrg <romanbezh@gmail.com>
2021-01-31 14:12:48 +0200

    Добавлена валидация формы по возрасту

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 
+from django.core.exceptions import ValidationError
+
 from .models import User, Customer
 
 
 class ProfileForm(forms.ModelForm):
-
+    age = forms.IntegerField()
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age < 18:
+            raise ValidationError('Возраст должен быть 18 и старше')
+        return age
+    
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+   
 
 class CustomerForm(forms.ModelForm):
 
