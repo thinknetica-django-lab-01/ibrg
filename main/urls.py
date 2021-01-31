2891132adc2c071bd11cf9fdbc7612baa20ea43e
ibrg <romanbezh@gmail.com>
2021-01-31 17:52:02 +0200

    добавление и редактирование объявлений

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import AdvertListView, AdvertDetailView, CustomerProfileUpdate
+from .views import AdvertListView, AdvertDetailView, CustomerProfileUpdate, ApartmentCreateView, HouseCreateView, AdvertUpdate
 
 
 urlpatterns = [
     path('', AdvertListView.as_view(), name='adverts-list'),
-    path('<slug:category_slug>/', AdvertListView.as_view(), name='adverts-list'),
     path('<int:pk>/', AdvertDetailView.as_view(), name='advert-detail'),
+    path('add/apartment/', ApartmentCreateView.as_view(), name='add_apartment'),
+    path('add/house/', HouseCreateView.as_view(), name='add_house'),
+    path('update/<slug:slug>/', AdvertUpdate.as_view(), name='update_advert'),
+    path('<slug:category_slug>/', AdvertListView.as_view(), name='adverts-list'),
+    
     path('accounts/profile/<int:pk>/', CustomerProfileUpdate.as_view(), name='update_profile'),
-
-
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, FormView, UpdateView
+from django.views.generic import ListView, DetailView, FormView, UpdateView, CreateView
 from django.contrib.auth.decorators import login_required
 
-from .models import Advert, Customer, User
+from .models import Advert, Apartment, House, Customer, User
 from .forms import ProfileForm
 
 
 class AdvertListView(ListView):
     model = Advert
-    paginate_by = 4
+    paginate_by = 10
     template_name = 'main/advert_list.html'
 
     def get_queryset(self):
@@ -16,12 +16,56 @@ class AdvertListView(ListView):
         if self.kwargs.get('category_slug'):
             queryset = queryset.filter(advert_category__category_slug=self.kwargs['category_slug']).order_by('-id')
         return queryset
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if not context.get('is_paginated', False):
+            return context
+
+        paginator = context.get('paginator')
+        num_pages = paginator.num_pages
+        current_page = context.get('page_obj')
+        page_no = current_page.number
+
+        if num_pages <= 11 or page_no <= 6:  # case 1 and 2
+            pages = [x for x in range(1, min(num_pages + 1, 12))]
+        elif page_no > num_pages - 6:  # case 4
+            pages = [x for x in range(num_pages - 10, num_pages + 1)]
+        else:  # case 3
+            pages = [x for x in range(page_no - 5, page_no + 6)]
+
+        context.update({'pages': pages})
+        return context
+
 
 
 class AdvertDetailView(DetailView):
     model = Advert
     template_name = 'main/advert_detail.html'
 
+
+class ApartmentCreateView(CreateView):
+    model = Apartment
+    template_name = 'main/forms.html'
+    fields ='__all__'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = "Новое объявление по продаже квартиры"
+        return context
+
+class HouseCreateView(CreateView):
+    model = House
+    template_name = 'main/forms.html'
+    fields ='__all__'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = "Новое объявление по продаже дома"
+        return context
 
 class CustomerProfileUpdate(UpdateView):
     model = User
@@ -37,7 +81,20 @@ class CustomerProfileUpdate(UpdateView):
         return super().form_valid(form)
 
 
-#
+
+class AdvertUpdate(UpdateView):
+    model = Advert
+    template_name = 'main/forms.html'
+    fields = '__all__'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = "Изменения объявления"
+        return context
+
 
 
 def index(request):
