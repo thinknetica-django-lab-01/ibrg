71a109b7d4ea2af3117e9562b0c16193cac47e64
ibrg <romanbezh@gmail.com>
2021-02-08 01:00:26 +0200

    Update email_config.py
    detele email conf

--- email_config.py
+++ email_config.py
@@ -1,6 +1,6 @@
-EMAIL_HOST = 'smtp.zoho.eu'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'noreply@agrowmp.com'
-EMAIL_HOST_PASSWORD = '_MeizuM5_2020'
-SERVER_EMAIL = 'noreply@agrowmp.com'
-DEFAULT_FROM_EMAIL = 'noreply@agrowmp.com'
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+SERVER_EMAIL = ''
+DEFAULT_FROM_EMAIL = ''
