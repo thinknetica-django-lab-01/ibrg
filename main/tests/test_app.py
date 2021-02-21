b74d83336925780c21b2f2b7a65cef9fbcfb1cc2
ibrg <romanbezh@gmail.com>
2021-02-21 22:14:04 +0200

    fix2 pytest

--- main/tests/test_app.py
+++ main/tests/test_app.py
@@ -1,13 +1,14 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
-django.setup()
-
 import pytest
 from pytest_django.asserts import assertTemplateUsed
 from django.urls import reverse
-from main.models import Advert, House
+from main.models import Advert
+from django.contrib.auth.models import User, Group
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+django.setup()
 
 
 @pytest.mark.django_db
@@ -15,36 +16,59 @@ class TestAdvertView:
 
     @pytest.fixture(scope="function")
     def set_obj(self):
-        for x in range(1,21):
-            obj = Advert.objects.create(
+        for x in range(1, 21):
+            Advert.objects.create(
                 advert_title=f'Advert title {x}',
                 slug=f'advert-slug-{x}',
                 rooms=2, price=20000, area=54, year=f'{2010 + 1}',
                 building_type='brick')
 
+    @pytest.fixture(scope="function")
+    def test_user(self, client):
+        """
+        Создаем пользователя та группы чтобы получить
+        доступ к добавлению обьявлений
+        """
+        username = "TT42"
+        password = "424242"
+        # Создаем группы
+        Group.objects.create(name='common_users')
+        Group.objects.create(name='realtor')
+        # создаем список с групами
+        user_group = Group.objects.filter(name__in=['realtor', 'common_users'])
+        # Создаем пользователя
+        User.objects.create(username=username, password=password)
+        self.user = User.objects.get(id=1)
+        # Добавляем пользователя в групы
+        self.user.groups.add(*user_group)
+
     def test_advert_count(self, set_obj):
         assert Advert.objects.count() == 20
 
-    def test_advert_house_create(self):
-        assert House.objects.count() == 0
-        obj = House.objects.create(
-            advert_title = 'House 42',
-            slug = 'house-42',
-            rooms = 42, price = 424242, 
-            area = 42, year=2042,
-            building_type = 'brick'
-        )
-        assert House.objects.count() == 1
-        assert obj.advert_title == 'House 42'
-        assert obj.price == 424242
+    def test_advert_house_create(self, test_user, client):
+        # Авторизуем созданого пользователя
+        client.force_login(self.user)
 
-     # Тестирование ответа сервера
+        url = reverse('add_house')
+        data = {
+            'advert_title': 'House 42',
+            'slug': 'house-42',
+            'rooms': 42,
+            'price': 424242,
+            'area': 42,
+            'year': 2042,
+            'building_type': 'brick'
+        }
+        obj = client.post(url, data)
+        assert obj.status_code == 200
+
+    # Тестирование ответа сервера
     def test_adverts_get_url(self, client):
         resp = client.get(reverse('adverts-list'))
         assert resp.status_code == 200
 
     def test_advert_detail(self, set_obj, client):
-        url = reverse('advert-detail', args = [10,])
+        url = reverse('advert-detail', args=[10, ])
         response = client.get(url)
         assert response.status_code == 200
 
@@ -59,5 +83,3 @@ class TestAdvertView:
         resp = client.get(reverse('advert-detail', args=[obj.pk]))
         assert resp.status_code == 200
         assertTemplateUsed(resp, 'main/advert_detail.html')
-
-
