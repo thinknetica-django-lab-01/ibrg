6849b94bef7b236145f1f5f9bab854fbd407b97b
ibrg <romanbezh@gmail.com>
2021-03-06 00:58:13 +0200

    add sitemap and robots

--- conf/settings.py
+++ conf/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'django.contrib.sitemaps',
 
     # vendors apps
     'ckeditor',
