cb35a5e8b1019a212a4609a8e9f476e0fe74d51e
ibrg <romanbezh@gmail.com>
2021-03-09 23:16:48 +0200

    CRUD

--- main/serializers.py
+++ main/serializers.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from .models import Advert
 
 
-class AdvertSerializer(serializers.HyperlinkedModelSerializer):
+class AdvertSerializer(serializers.ModelSerializer):
+
+    # Корректно работает через Postman
+    advert_category = serializers.ListField(
+        child=serializers.CharField(max_length=100, allow_blank=True, allow_null=True, required=False))
 
     class Meta:
         model = Advert
-        fields = ['url',
-                  'advert_title',
+        fields = ['advert_title',
                   'slug',
                   'advert_category',
                   'rooms', 'price', 'area',
--- main/views.py
+++ main/views.py
@@ -12,7 +12,7 @@ from django.views.decorators.cache import cache_page
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.contrib.postgres.search import SearchVector
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import AdvertSerializer
 
 from .forms import ProfileForm, SubscribeForm, UserForm
@@ -170,3 +170,4 @@ class AdvertViewSet(viewsets.ModelViewSet):
     """
     queryset = Advert.objects.all()
     serializer_class = AdvertSerializer
+    permission_classes = [permissions.IsAuthenticated]
